d2e90ac1738e8287d5dcfc65e751465335adb370
tristan <tristan.hacquard@polytechnique.org>
2024-05-08 12:16:20 +0200

    feature: added anakin_model as exported symbol

--- src/MLNanoShaperRunner.jl
+++ src/MLNanoShaperRunner.jl
@@ -1,6 +1,8 @@
 module MLNanoShaperRunner
+export RegionMesh,distance,signed_distance,anakin_model
 
 include("c_interface.jl")
 include("distance_tree.jl")
 include("layers.jl")
+include("models.jl")
 end # module MLNanoShaperRunner
---
+++ src/models.jl
@@ -0,0 +1,13 @@
+using Lux
+
+function anakin_model()
+    a = 5
+    b = 5
+    adaptator = ToSimpleChainsAdaptor((static(a * b + 2),))
+    chain = Chain(Dense(a * b + 2 => 10,
+            elu),
+        Dense(10 => 1, elu;
+            init_weight = (args...) -> glorot_uniform(args...; gain = 1 / 25_0000)))
+    Lux.Chain(preprocessing, struct_stack,
+        DeepSet(Chain(Encoding(a, b, 1.5f0), chain)), tanh_fast)
+end
