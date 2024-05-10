34e19e4a472347c1048be23e659deaa4b1636319
tristan <tristan.hacquard@polytechnique.org>
2024-05-10 18:29:26 +0200

    feature: added a module for all models

--- src/models.jl
+++ src/models.jl
@@ -1,13 +1,20 @@
 using Lux
+module Models
+"""
+    anakin()
 
-function anakin_model()
+A model inspired by the amakin paper.
+The protein is decomposed in pairs of atom near the interest point.
+Each pair of atom is turned into a `Preprocessed` data and is then fed to the `Encoding` layer. 
+"""
+function anakin()
     a = 5
     b = 5
-    adaptator = ToSimpleChainsAdaptor((static(a * b + 2),))
     chain = Chain(Dense(a * b + 2 => 10,
             elu),
         Dense(10 => 1, elu;
             init_weight = (args...) -> glorot_uniform(args...; gain = 1 / 25_0000)))
     Lux.Chain(preprocessing,
         DeepSet(Chain(Encoding(a, b, 1.5f0), chain)), tanh_fast)
+end
 end
