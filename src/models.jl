887d487b4df3651b01a3cf065445be47401bc258
tristan <tristan.hacquard@polytechnique.org>
2024-05-16 15:51:44 +0200

    .

--- src/models.jl
+++ src/models.jl
@@ -6,7 +6,7 @@ A model inspired by the anakin paper.
 The protein is decomposed in pairs of atom near the interest point.
 Each pair of atom is turned into a `Preprocessed` data and is then fed to the `Encoding` layer. 
 """
-function anakin(; cutoff_radius::Float32=3.0f0)
+function anakin(; cutoff_radius::Float32=3f0)
     a = 5
     b = 5
     chain = Chain(Dense(a * b + 2 => 10,
@@ -17,7 +17,7 @@ function anakin(; cutoff_radius::Float32=3.0f0)
         DeepSet(Chain(gpu_device(),Encoding(a, b, cutoff_radius), chain)), tanh_fast;name="anakin")
 end
 
-function angular_dense(; cutoff_radius::Float32=3.0f0)
+function angular_dense(; cutoff_radius::Float32=3f0)
     chain = Chain(Dense(5 => 10,elu),
         Dense(10 => 1, elu;
             init_weight=(args...) -> glorot_uniform(args...; gain=1 / 25_0000)))
