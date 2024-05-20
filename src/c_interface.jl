104290fce9f88812b236769719d2e5d4bb954f72
tristan <tristan.hacquard@polytechnique.org>
2024-05-20 14:37:29 +0200

    feature: implemented c_interface

--- src/c_interface.jl
+++ src/c_interface.jl
@@ -1,4 +1,8 @@
 using NearestNeighbors
+using Serialization
+using Lux
+using GeometryBasics
+using StructArrays
 
 """
 	state
@@ -7,18 +11,21 @@ The global state manipulated by the c interface.
 To use, you must first load the weights using `load_weights` and the input atoms using `load_atoms`.
 Then you can call eval_model to get the field on a certain point.
 """
-mutable struct state
-	weights::NamedTuple
-	atoms::KDTree
+Option{T} = Union{T, Nothing}
+mutable struct State
+    weights::Option{Lux.Experimental.TrainState}
+    atoms_tree::Option{KDTree{Point3f}}
+    atoms::Option{StructVector{Sphere{Float32}}}
+	cutoff_radius::Float32
 end
+global_state = State(nothing, nothing, nothing,3.0)
 
 struct CSphere
-	x::Float32
-	y::Float32
-	z::Float32
-	r::Float32
+    x::Float32
+    y::Float32
+    z::Float32
+    r::Float32
 end
-
 
 """
     load_weights(path::String)::Int
@@ -32,7 +39,24 @@ Load the model `parameters` and `model` from a serialised training state at abso
 - 3: unknow error
 """
 function load_weights(path::String)::Int
-	
+    try
+        if ispath(path)
+            data = deserialize(path)
+            if typeof(data) <: Lux.Experimental.TrainState
+                global_state.weights = deserialize(path)
+                0
+            else
+                @error "file not found" path
+                2
+            end
+        else
+            @error "wrong type, expected Lux.Experimental.TrainState, got" typeof(data)
+            1
+        end
+    catch err
+        @error err
+        3
+    end
 end
 
 """
@@ -46,8 +70,16 @@ Start is a pointer to the start of the array of `CSphere` and `length` is the le
 - 1: data could not be read
 - 2: unknow error
 """
-function load_atoms(start::Ptr{CSphere},length::UInt64)::Int
-	
+function load_atoms(start::Ptr{CSphere}, length::UInt64)::Int
+    try
+        data = Iterators.map(unsafe_wrap(Array, start, length)) do (; x, y, z, r)
+            Sphere(Point3f(x, y, z), r)
+        end |> StridedVector
+        global_state.atoms = KDTree(data; reorder = false)
+    catch err
+        @error err
+        2
+    end
 end
 
 """
@@ -55,11 +87,14 @@ end
 
 evaluate the model at coordinates `x` `y` `z`.
 """
-function eval_model(x::Float32,y::Float32,z::Float32)::Float32
-	
+function eval_model(x::Float32, y::Float32, z::Float32)::Float32
+    point = Point3f(x, y, z)
+    neighbors = global_state.atoms[inrange(
+        global_state.atoms_tree, point, global_state.cutoff_radius)]
+    Lux.eval(global_state.weights.model, ModelInput(point, neighbors),
+        global_state.weights.parameters, global_state.weights.state)
 end
 
 @cfunction load_weights Int (String,)
-@cfunction load_atoms Int (Ptr{CSphere},UInt32)
-@cfunction eval_model Float32 (Float32,Float32,Float32)
-
+@cfunction load_atoms Int (Ptr{CSphere}, UInt32)
+@cfunction eval_model Float32 (Float32, Float32, Float32)
