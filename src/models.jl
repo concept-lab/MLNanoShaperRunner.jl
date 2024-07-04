e6ab0613651d049555f7f286b3cdccf933aa8775
tristan <tristan.hacquard@polytechnique.org>
2024-07-04 14:44:53 +0200

    fix:fixed batch preprocessing

--- src/models.jl
+++ src/models.jl
@@ -2,7 +2,7 @@ using Lux
 
 select_and_preprocess((point, atoms); cutoff_radius) =select_and_preprocess(point,atoms;cutoff_radius)
 function select_and_preprocess(point::Batch, atoms; cutoff_radius)
-	Batch(select_and_preprocess.(point,Ref(atoms);cutoff_radius))
+	Batch(select_and_preprocess.(point.field,Ref(atoms);cutoff_radius))
 end
 function select_and_preprocess(point::Point, atoms; cutoff_radius)
     atoms = select_neighboord(point, atoms; cutoff_radius)
