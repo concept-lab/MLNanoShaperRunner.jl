d31c979c794db0bf0269bbbde677f6fb0a934f44
Tristan Hacquard <tristan.hacquard@polytechnique.org>
2025-02-24 13:27:36 +0100

    feature: can remove smoothing

--- src/models.jl
+++ src/models.jl
@@ -30,11 +30,27 @@ function evaluate_if_atoms_in_neighboord(layer, arg::AbstractArray, ps, st; zero
     end
 end
 
+function smoothing_layer(layer)
+    Parallel(
+        .*,
+        layer,
+        Lux.WrappedFunction(scale_factor),
+    )
+end
+
+function add_van_der_waals_channel(main_chain)
+        Parallel(
+            vcat,
+            main_chain,
+            WrappedFunction((x -> Float32.(x)) ∘ is_in_van_der_waals),
+        )
+    end
 function general_angular_dense(
     main_chain,
     secondary_chain;
     name::String,
     van_der_waals_channel::Bool = false,
+    smoothing::Bool = true,
     on_gpu::Bool = true,
     cutoff_radius::Float32 = 3.0f0,
 )
@@ -44,16 +60,11 @@ function general_angular_dense(
             main_chain,
         ),
     )
-    function add_van_der_waals_channel(main_chain)
-        Parallel(
-            vcat,
-            main_chain,
-            WrappedFunction((x -> Float32.(x)) ∘ is_in_van_der_waals),
-        )
-    end
-    Chain(
+        Chain(
         PreprocessingLayer(Partial(select_and_preprocess; cutoff_radius)),
-        main_chain |> (van_der_waals_channel ? add_van_der_waals_channel : identity),
+        main_chain
+            |> (smoothing ? smoothing_layer : identity)
+            |> (van_der_waals_channel ? add_van_der_waals_channel : identity),
         secondary_chain;
         name,
     )
@@ -65,57 +76,64 @@ end
 	`tiny_angular_dense` is a function that generate a lux model.
 
 """
-function tiny_angular_dense(; van_der_waals_channel = false, kargs...)
+function tiny_angular_dense(;
+    van_der_waals_channel = false,
+    smoothing = true,
+    kargs...)
     general_angular_dense(
-        Parallel(
-            .*,
-            Chain(Dense(6 => 7, elu), Dense(7 => 4, elu)),
-            Lux.WrappedFunction(scale_factor),
-        ),
+        Chain(Dense(6 => 7, elu), Dense(7 => 4, elu)),
         Chain(
             BatchNorm(4 + van_der_waals_channel),
             Dense(4 + van_der_waals_channel => 6, elu),
             Dense(6 => 1, sigmoid_fast),
         );
-        name = "tiny_angular_dense" * (van_der_waals_channel ? "_v" : ""),
+        name = "tiny_angular_dense" *
+            (van_der_waals_channel ? "_v" : ""),
+            (smoothing ? "_s" : ""),
         van_der_waals_channel,
+        smoothing,
         kargs...,
     )
 end
 
-function light_angular_dense(; van_der_waals_channel = false, kargs...)
+function light_angular_dense(;
+    van_der_waals_channel = false,
+    smoothing =true,
+    kargs...
+)
     general_angular_dense(
-        Parallel(
-            .*,
-            Chain(Dense(6 => 10, elu), Dense(10 => 5, elu)),
-            Lux.WrappedFunction(scale_factor),
-        ),
+        Chain(Dense(6 => 10, elu), Dense(10 => 5, elu)),
         Chain(
             BatchNorm(5 + van_der_waals_channel),
             Dense(5 + van_der_waals_channel => 10, elu),
             Dense(10 => 1, sigmoid_fast),
         );
-        name = "light_angular_dense" * (van_der_waals_channel ? "_v" : ""),
+        name = "light_angular_dense" *
+            (van_der_waals_channel ? "_v" : "") *
+            (smoothing ? "_s" : ""),
         van_der_waals_channel,
+        smoothing,
         kargs...,
     )
 end
 
-function medium_angular_dense(; van_der_waals_channel = false, kargs...)
+function medium_angular_dense(;
+    van_der_waals_channel = false,
+    smoothing = true,
+    kargs...)
     general_angular_dense(
-        Parallel(
-            .*,
-            Chain(Dense(6 => 15, elu), Dense(15 => 10, elu)),
-            Lux.WrappedFunction(scale_factor),
-        ),
+        Chain(Dense(6 => 15, elu), Dense(15 => 10, elu)),
         Chain(
             BatchNorm(10 + van_der_waals_channel),
             Dense(10 + van_der_waals_channel => 5; use_bias = false),
             Dense(5 => 10, elu),
             Dense(10 => 1, sigmoid_fast),
         );
-        name = "medium_angular_dense" * (van_der_waals_channel ? "_v" : ""),
+        name = "medium_angular_dense" *
+            (van_der_waals_channel ? "_v" : ""),
+            (smoothing ? "_s" : ""),
         van_der_waals_channel,
+        smoothing,
         kargs...,
     )
 end
