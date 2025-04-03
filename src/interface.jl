ce664a4e7e2e261d1633998b493f1ac4a62a14fb
Tristan Hacquard <tristan.hacquard@polytechnique.org>
2025-04-03 11:46:02 +0200

    change: changed bach size from 2 to 1000 in surface computation

--- src/interface.jl
+++ src/interface.jl
@@ -4,7 +4,7 @@ function evaluate_field(model::StatefulLuxLayer,atoms::RegularGrid;step::Number=
     ranges = range.(mins, maxes; step)
     grid = Point3f.(reshape(ranges[1], :, 1,1), reshape(ranges[2], 1, :,1), reshape(ranges[3], 1,1,:))
     g = reshape(grid,:)
-    batch_size = 2
+    batch_size = 1000
     volume = similar(grid,Float32)
     v = reshape(volume,:)
     for i in 1:batch_size:length(volume)
